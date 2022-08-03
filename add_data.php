a3c43e71147a27422bcf3d2fe66ee9d980b7fa3f
anwar <76504162+anwar-gekha@users.noreply.github.com>
2022-08-03 16:50:28 +0700

    update add_data.php

--- add_data.php
+++ add_data.php
@@ -48,7 +48,7 @@ $sql_query="INSERT INTO data (`id`,`name_doc`,`expired`,`pic`,`keterangan`) VALU
 <center>
 
 <div id="container"> <div id="table-responsive">
-        <label>Core PHP Crud functions - <a href="http://www.phpcodebuilder.com" target="_blank">By PHP Code Builder</a></label>
+                <label>Core PHP Crud functions - <a href="" target="_blank">Code Builder</a></label>
     </div>
 </div>
 <div id="container"> &<div   id="table-responsive">
